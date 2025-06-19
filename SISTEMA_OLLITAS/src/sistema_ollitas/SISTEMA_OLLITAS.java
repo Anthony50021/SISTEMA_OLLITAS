e57d71b30f6125be0b92157a4ed6ae247a408d88
Bryan Mercado <bm470017@gmail.com>
2025-06-18 19:56:54 -0500

    Primer commit

---
+++ SISTEMA_OLLITAS/src/sistema_ollitas/SISTEMA_OLLITAS.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sistema_ollitas;
+
+/**
+ *
+ * @author Juan Daniel
+ */
+public class SISTEMA_OLLITAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Pongame 20");
+    }
+    
+}
